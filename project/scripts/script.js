681893e2842d7df269efddb2acf6fd1538e3063d
Alexander Dullinger <a.dullinger@students.htl-leonding.ac.at>
2026-03-27 12:24:59 +0100

    Navigation

---
+++ project/scripts/script.js
@@ -0,0 +1,7 @@
+let block1 = document.getElementById('block-1')
+let block2 = document.getElementById('block-2')
+
+function showBlockTwo(){
+    block1.style.display = 'none'
+    block2.style.display = 'grid'
+}
