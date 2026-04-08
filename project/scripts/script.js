7017070490005283e1c94e118a03c4ce53d04b05
Alexander Dullinger <a.dullinger@students.htl-leonding.ac.at>
2026-04-08 14:01:50 +0200

    erklär html

--- project/scripts/script.js
+++ project/scripts/script.js
@@ -1,3 +1,4 @@
+// index
 let block1 = document.getElementById('block-1')
 let block2 = document.getElementById('block-2')
 
@@ -5,3 +6,15 @@ function showBlockTwo(){
     block1.style.display = 'none'
     block2.style.display = 'grid'
 }
+
+// erklär
+let EBlockEins = document.getElementById('E-block-eins')
+let EblockZwei = document.getElementById('E-block-zwei')
+let erklaerText = document.getElementById('erklaer-text')
+
+function showEBlockTwo(){
+    EBlockEins.style.display = 'none'
+    EblockZwei.style.display = 'grid'
+}
+
+
